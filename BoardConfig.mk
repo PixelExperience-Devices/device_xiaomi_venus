ae6ff23444a37301bcc15f8a19d9e0cfaa3ac057
Chenyang Zhong <zhongcy95@gmail.com>
2022-05-03 04:23:59 -0400

    venus: specify kernel source path
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ TARGET_SCREEN_DENSITY := 560
 TARGET_OTA_ASSERT_DEVICE := venus
 
 # Kernel
+TARGET_KERNEL_SOURCE := kernel/xiaomi/venus
 TARGET_KERNEL_CONFIG := vendor/venus-qgki_defconfig
 
 # Kernel modules
