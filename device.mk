a938bce23906e37b3894490a8d06ccb292916e4e
Ramii Ahmed <ramy@ahmedramy.com>
2023-02-18 21:58:20 +0000

    venus: Device has udfps
    Change-Id: Ifcee606d378d933a442f2da3ef86686076257f3f

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 # Inherit from sm8350-common
 $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    libudfpshandler \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
