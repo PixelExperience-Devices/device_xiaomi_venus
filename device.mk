4be1f65c59fba3f2541c37f0ff2718ec2e47d089
FlowerSea0208 <zzp060208@gmail.com>
2023-06-10 20:59:32 +0800

    venus: Build common Xiaomi PowerShare HIDL
    Change-Id: Iae000afede3985a99b72b7985092bcdaf1abfa22

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.xiaomi
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.xiaomi.venus
